f59fa2c7f3b05894b3b4c658dfe77d8deeb006ef
Orlando Timmerman <rt582@cam.ac.uk>
2023-04-11 20:59:58 +0100

    added tqdm progress bar functions for downloading files

--- coralshift/utils/file_ops.py
+++ coralshift/utils/file_ops.py
@@ -2,17 +2,31 @@ from pathlib import Path
 
 def guarantee_existence(path: str) -> Path:
     """Checks if string is an existing path, else creates it
-	
-	Parameter
-	---------
-	path : str
-	
-	Returns
-	-------
-	Path
-		pathlib.Path object of path
-	"""
-	path_obj = Path(path)
+    
+    Parameter
+    ---------
+    path : str
+    
+    Returns
+    -------
+    Path
+        pathlib.Path object of path
+    """
+    path_obj = Path(path)
     if not path_obj.exists():
         path_obj.mkdir(parents=True)
     return path_obj.resolve()
+
+
+class DownloadProgressBar(tqdm) -> None:
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)
+
+
+def download_url(url, output_path, loading_bar: bool = True) -> None:
+    print('\n')
+    with DownloadProgressBar(unit='B', unit_scale=True,
+                             miniters=1, desc=url.split('/')[-1]) as t:
+        urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
