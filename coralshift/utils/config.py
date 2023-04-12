544fdf099c4f8cb00bfe939f7a6ec6aee6b1b85a
Orlando Timmerman <rt582@cam.ac.uk>
2023-04-12 23:35:41 +0100

    deleted spare config.py hanging around looking shady

--- coralshift/utils/config.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import importlib
-import inspect
-
-
-def get_h3_dir():
-    coralshift_module = importlib.import_module("coralshift")
-    coralshift_dir = Path(inspect.getabsfile(coralshift_module)).parent
-    return (coralshift_dir / "..").resolve()
