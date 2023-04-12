ee2eddfa62ff07ac5049beb2fc52e525c6de11de
Orlando Timmerman <rt582@cam.ac.uk>
2023-04-12 11:03:24 +0100

    formatting

--- coralshift/utils/config.py
+++ coralshift/utils/config.py
@@ -4,6 +4,6 @@ import inspect
 
 
 def get_h3_dir():
-	coralshift_module = importlib.import_module("coralshift")
-	coralshift_dir = Path(inspect.getabsfile(coralshift_module)).parent
-	return (coralshift_dir / "..").resolve()
+    coralshift_module = importlib.import_module("coralshift")
+    coralshift_dir = Path(inspect.getabsfile(coralshift_module)).parent
+    return (coralshift_dir / "..").resolve()
