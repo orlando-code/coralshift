0d566ec0798ac643b3ff8804ff5d5f2ebd62f987
Orlando Timmerman <rt582@cam.ac.uk>
2023-04-12 12:04:51 +0100

    added get_n_last_subparts_path and check_path_suffix with a little cleaning

--- coralshift/utils/file_ops.py
+++ coralshift/utils/file_ops.py
@@ -33,3 +33,34 @@ def download_url(url, output_path, loading_bar: bool = True) -> None:
     with DownloadProgressBar(unit='B', unit_scale=True,
                              miniters=1, desc=url.split('/')[-1]) as t:
         urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
+
+
+def get_n_last_subparts_path(path: Path | str, n: int) -> Path:
+    """Returns 'n' last parts of a path. E.g. /first/second/third/fourth with n = 3 will return second/third/fourth"""
+    return Path(*Path(path).parts[-n:])
+
+
+def check_path_suffix(path: Path | str, comparison: str) -> bool:
+    """Checks whether path provided ends in a particular suffix e.g. "nc". Since users usually forget to specify ".",
+    pads "comparison" with a period if missing.
+
+    Parameters
+    ----------
+    path (Path | str): path to have suffix checked
+    comparison (str): extension to check for
+
+    Returns
+    -------
+    bool: True if file path extension is equal to comparison, False otherwise"""
+    p = Path(path)
+    if not p.is_file():
+        raise ValueError(f"{path} is not a file path.")
+
+    # pad with leading "."
+    if "." not in comparison:
+        comparison = "." + comparison
+
+    if p.suffix == comparison:
+        return True
+    else:
+        return False
