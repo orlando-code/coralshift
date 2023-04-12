25c90f2aeda11e70d8851762127225c569bd4568
Orlando Timmerman <rt582@cam.ac.uk>
2023-04-12 23:35:16 +0100

    replaced os with pathlib functions, updated function name

--- coralshift/config.py
+++ coralshift/config.py
@@ -1,10 +1,9 @@
-import os
 import importlib
 import inspect
-# N.B. not too sure about this functionality: just reproduced from h3
+from pathlib import Path
 
 
 def get_coralshift_dir():
     coralshift_module = importlib.import_module("coralshift")
-    coralshift_dir = os.path.dirname(inspect.getabsfile(coralshift_module))
-    return os.path.abspath(os.path.join(coralshift_dir, ".."))
+    coralshift_dir = Path(inspect.getabsfile(coralshift_module)).parent
+    return (coralshift_dir / "..").resolve()
