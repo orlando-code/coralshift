57eeffc87dcbd11e90089d71fb40952e832f7367
Orlando Timmerman <rt582@cam.ac.uk>
2023-05-02 22:01:52 +0100

    fixed Path arg and changed name to specify data

--- coralshift/utils/directories.py
+++ coralshift/utils/directories.py
@@ -7,26 +7,26 @@ from pathlib import Path
 #     return guarantee_existence(get_coralshift_dir(), 'data')
 
 
-def get_drive_dir(volume_name: str) -> Path:
+def get_volume_dir(volume_name: str) -> Path:
     """~/Volumes/volume_name"""
-    return Path("/Volumes" / volume_name)
+    return Path("/Volumes", volume_name)
 
 
 def get_datasets_dir() -> Path:
     """./data/datasets"""
-    return guarantee_existence(get_coralshift_dir() / 'datasets')
+    return guarantee_existence(get_coralshift_dir() / "datasets")
 
 
 def get_reef_baseline_dir() -> Path:
     """./data/datasets/reef_baseline"""
-    return guarantee_existence(get_datasets_dir() / 'reef_baseline')
+    return guarantee_existence(get_datasets_dir() / "reef_baseline")
 
 
 def get_bathymetry_datasets_dir() -> Path:
     """./datasets/bathymetry"""
-    return guarantee_existence(get_datasets_dir() / 'bathymetry')
+    return guarantee_existence(get_datasets_dir() / "bathymetry")
 
 
 def get_gbr_bathymetry_data_dir() -> Path:
     """./datasets/bathymetry/GBR_30m"""
-    return guarantee_existence(get_bathymetry_datasets_dir() / 'GBR_30m')
+    return guarantee_existence(get_bathymetry_datasets_dir() / "GBR_30m")
