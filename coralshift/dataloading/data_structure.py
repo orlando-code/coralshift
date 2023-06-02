9300bd321faf657f1e5e3687ccac062d274ec1ce
Orlando Timmerman <rt582@cam.ac.uk>
2023-06-02 14:25:01 +0100

    adding dataset management class

---
+++ coralshift/dataloading/data_structure.py
@@ -0,0 +1,40 @@
+import os
+
+from pathlib import Path
+from coralshift.utils import directories
+
+
+class MyDatasets:
+    """Handle the variety of datasets required to test and train model"""
+
+    # TODO: add in declaration of filepath root
+    def __init__(self):
+        self.datasets = {}
+        self.files_location = Path()
+        # fetching external functions
+
+    def set_location(self, location="remote"):
+        if location == "remote":
+            # change directory to home. TODO: make less hacky
+            os.chdir("/home/jovyan")
+            self.files_location = Path("lustre_scratch/datasets/")
+        elif location == "local":
+            self.files_location = directories.get_volume_dir()
+        else:
+            raise ValueError
+
+    def get_location(self):
+        return self.files_location
+
+    def add_dataset(self, name, data):
+        self.datasets[name] = data
+
+    def get_dataset(self, name):
+        return self.datasets.get(name, None)
+
+    def remove_dataset(self, name):
+        if name in self.datasets:
+            del self.datasets[name]
+
+    def list_datasets(self):
+        return list(self.datasets.keys())
