036ca9becc79cf15e8376c2860b3ae536ea5d728
Orlando Timmerman <rt582@cam.ac.uk>
2023-06-03 22:41:06 +0100

    small quantity of documentation

--- coralshift/dataloading/data_structure.py
+++ coralshift/dataloading/data_structure.py
@@ -14,29 +14,32 @@ class MyDatasets:
         # fetching external functions
 
     def set_location(self, location: str = "remote", volume_name: str = "MRes Drive"):
+        """Required to go between accessing data on remote (MAGEO) and local (usually
+        external harddrive)
+        """
         if location == "remote":
             # change directory to home. TODO: make less hacky
             os.chdir("/home/jovyan")
             self.files_location = Path("lustre_scratch/datasets/")
         elif location == "local":
-            self.files_location = directories.get_volume_dir(volume_name)
+            self.files_location = directories.get_volume_dir(volume_name) / "datasets/"
         else:
-            raise ValueError
+            raise ValueError(f"Unrecognised location: {location}")
 
     def get_location(self):
         return self.files_location
 
-    def add_dataset(self, name, data):
+    def add_dataset(self, name: str, data):
         self.datasets[name] = data
 
-    def add_datasets(self, names, data):
+    def add_datasets(self, names: list[str], data: list):
         for i, name in enumerate(names):
             self.datasets[name] = data[i]
 
-    def get_dataset(self, name):
+    def get_dataset(self, name: str):
         return self.datasets.get(name, None)
 
-    def remove_dataset(self, name):
+    def remove_dataset(self, name: str):
         if name in self.datasets:
             del self.datasets[name]
 
