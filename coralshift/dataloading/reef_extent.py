af2b8c45fa61b5841eec721742512deee3fdfcc0
Orlando Timmerman <rt582@cam.ac.uk>
2023-06-12 01:25:24 +0100

    added process_benthic_df and pandas import

--- coralshift/dataloading/reef_extent.py
+++ coralshift/dataloading/reef_extent.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 from pathlib import Path
 
 
@@ -51,3 +52,29 @@ def generate_area_geojson(area_class, area_name: str, save_dir: Path | str) -> N
         json.dump(geojson_data, file)
 
     return output_path
+
+
+def process_benthic_pd(
+    benthic_df: pd.DataFrame, limit_to: list[str] = ["Coral/Algae"]
+) -> pd.DataFrame:
+    # don't overwite original values
+    working_df = benthic_df.copy()
+
+    # define Coral/Algae as target
+    class_vals = {
+        "Coral/Algae": 1,
+        "Reef": 2,
+        "Rock": 3,
+        "Rubble": 4,
+        "Sand": 5,
+        "Microalgal Mats": 6,
+        "Seagrass": 7,
+    }
+    working_df["class_val"] = working_df["class"].map(class_vals)
+
+    # Convert the values in "class_val" column to integers
+    working_df["class_val"] = working_df["class_val"].astype(int)
+
+    # Filter the DataFrame to include only the specified classes
+    filtered_df = working_df[working_df["class"].isin(limit_to)]
+    return filtered_df
