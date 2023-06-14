3640756461eff1005b753af96ff7b796c74ddbf4
Orlando Timmerman <rt582@cam.ac.uk>
2023-06-14 11:45:52 +0100

    changed default arg to buffer_size=3, added nc suffix

--- coralshift/processing/spatial_data.py
+++ coralshift/processing/spatial_data.py
@@ -1581,14 +1581,14 @@ def spatially_buffer_timeseries(
     return buffered_ds
 
 
-def spatially_buffer_nc_file(nc_path: Path | str, buffer_size: int = 1):
+def spatially_buffer_nc_file(nc_path: Path | str, buffer_size: int = 3):
     # TODO: specify distance buffer
     nc_path = Path(nc_path)
     buffered_ds = spatially_buffer_timeseries(
         xa.open_dataset(nc_path), buffer_size=buffer_size
     )
     buffered_name = nc_path.stem + f"_buffered_{buffer_size}_pixel"
-    buffered_path = nc_path.parent / buffered_name
+    buffered_path = (nc_path.parent / buffered_name).with_suffix(".nc")
     buffered_ds.to_netcdf(buffered_path)
 
     return buffered_ds, buffered_path
